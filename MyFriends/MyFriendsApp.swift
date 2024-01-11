d30e195ca6daaea2d51bfd4fccb58aa1d212c80b
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-11 17:46:05 +0400

    Initial Commit

---
+++ MyFriends/MyFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFriendsApp.swift
+//  MyFriends
+//
+//  Created by Kristina Korotkova on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
